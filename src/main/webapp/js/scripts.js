3da651d0d081a82ddf13d0589ea0b09759e400ef
MFrizo <matheusfrizo@gmail.com>
2019-12-07 22:43:23 -0300

    Adds Front End Configuration files

---
+++ src/main/webapp/js/scripts.js
@@ -0,0 +1,15 @@
+function buscaEndereco(){
+    var cep = document.getElementById("cep").value;
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://cep.republicavirtual.com.br/web_cep.php?cep="+cep+"&formato=json";
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.status === 200){
+            if (xmlhttp.readyState === 4){
+                alert(xmlhttp.responseText);
+            }
+        }
+    };
+    xmlhttp.open("GET",url, true);
+    xmlhttp.send();
+    
+}
